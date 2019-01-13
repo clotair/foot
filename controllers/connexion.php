34fc48f86056e504d37760ae7e39e9cd040855e4
fez2000 <eugenefezeu@gmail.com>
2019-01-13 16:47:39 +0100

    satut

--- controllers/connexion.php
+++ controllers/connexion.php
@@ -1,6 +1,36 @@
 <?php 
-  require_once fontions.php;
+ function json_response($message = null, $code = 200)
+ {
+     // clear the old headers
+     header_remove();
+     // set the actual code
+     http_response_code($code);
+     // set the header to make sure cache is forced
+     header("Cache-Control: no-transform,public,max-age=300,s-maxage=900");
+     // treat this as json
+     header('Content-Type: application/json');
+     $status = array(
+         200 => '200 OK',
+         400 => '400 Bad Request',
+         422 => 'Unprocessable Entity',
+         500 => '500 Internal Server Error'
+         );
+     // ok, validation error, or failure
+     header('Status: '.$status[$code]);
+     // return the encoded json
+     return json_encode(array(
+         'status' => $code < 300, // success or not?
+         'message' => $message
+         ));
+ }
   require '../inc/db.php';
+  
+  /*$method = $_SERVER['REQUEST_METHOD'];
+  $request = explode('/',trim($_server['PATH_INFO']))
+  $input = json_decode(file_get_contents('php://input'),true)
+  */
+  echo json_response($_POST,200);
+  /*
   if(isset($_POST['nom']){
       echo 'require nom';
   }else{
@@ -27,5 +57,5 @@
           }
       }  )
       
-  }
+  }*/
 ?>
