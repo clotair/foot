d388af0f13a66c7e10338d5d09f25d36638e727f
German2018 <germantsakou@gmail.com>
2019-01-14 09:57:03 +0100

    change

--- controllers/upmatch.php
+++ controllers/upmatch.php
@@ -6,7 +6,7 @@
       if(!isset($_SESSION['connecter'])){
          $sql = 'UPDATE `matchs` SET `but1`=:bu1,`but2`=:bu2,`status`=:stat WHERE `equipe1`=:eq1 AND `equipe2`=:eq2';
          $query=$pdo->prepare($sql);
-         $query->execute(array('bu1'=>$_POST['but1'], 'bu2'=>$_POST['but2'],'stat'=1,'eq1'=$_POST['equipe1'],'eq2'=$_POST['equipe2']);
+         $query->execute(array('bu1'=>$_POST['but1'], 'bu2'=>$_POST['but2'],'stat'=>1,'eq1'=>$_POST['equipe1'],'eq2'=>$_POST['equipe2']));
          echo 'true';
       }else{
         echo 'false';
